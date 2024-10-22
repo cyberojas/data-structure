5cdbb50a27be50b484f9205488adec3a2d7a38eb
oju <120483267+cyberojas@users.noreply.github.com>
2024-10-22 19:48:26 +0530

    Update circularQueue.c

--- circularQueue.c
+++ circularQueue.c
@@ -1,83 +1,123 @@
-#include <stdio.h>
-#include <stdbool.h>
-
+#include<stdio.h>
+#include<stdbool.h>
 int queue[5];
-int size_max = 10;
-int front = -1;
-int rear = -1;
-
+int size_max=5;
+int front;
+int rear;
 void enqueue(int element);
 void dequeue();
+int getfront();
+int getrear();
 void viewqueue();
 bool isfull();
 bool isempty();
 
-int main() {
-    // Enqueue elements
-    enqueue(10);
-    enqueue(20);
-    enqueue(30);
-    enqueue(40);
-    enqueue(50);
 
-    // Dequeue an element
-    dequeue();
-    enqueue(99);
-    // View the queue
-    viewqueue();
-
-    return 0;
+int main()
+{
+	front=-1;
+	rear=-1;
+		
+		
+        	enqueue(10);
+	    	enqueue(20);
+			enqueue(30);
+			enqueue(40);
+			enqueue(50);
+				dequeue();
+				enqueue(99);
+			int a=getfront();
+			int b=getrear();
+			printf("\n Front element is = %0d",a);
+			printf("\n Rear element is = %0d",b);
+			viewqueue();
+			return 0;
 }
 
-void enqueue(int element) {
-    if (isfull()) {
-        printf("\nQueue is full");
-    } else {
-        if (front == -1) {  // First element being enqueued
-            front = 0;
-        }
-        rear = (rear + 1) % size_max;  // Circular increment
-        queue[rear] = element;
-        printf("\nEnqueue %d", queue[rear]);
-    }
+void enqueue(int element)
+{
+	if(rear==-1 && front==-1)
+	{
+		rear=0;
+		front=0;
+		queue[rear]=element;
+		printf("\n enqueue %d",queue[rear]);
+	}
+	else if(rear==size_max-1 && front==0)
+	{
+		printf("\n queue is full");
+	}
+	else if(rear==size_max-1 && front!=0)
+	{
+		rear=0;
+		queue[rear]=element;
+		printf("\n enqueue %d",queue[rear]);
+	}
+	else
+	{
+		rear=rear+1;
+		queue[rear]=element;
+		printf("\n enqueue %d",queue[rear]);
+	}
+
 }
 
-void dequeue() {
-    if (isempty()) {
-        printf("\nQueue is empty.");
-    } else {
-        printf("\nDequeue %d", queue[front]);
-        queue[front] = 0;
+void dequeue()
+{
+	if(front==-1)
+	{
+		printf("\n queue is empty..");
+	}
+	else
+	{
+		printf("\n dequeue %d",queue[front]);
+		queue[front]=0;
+		if(front==rear)
+		{
+			front=-1;
+			rear=-1;
+		}
+		else if(front==size_max-1)
+		{
+			front=0;
+		}
+		else
+		{
+			front=front+1;
+		}
+	}
+}
 
-        if (front == rear) {
-            // Queue becomes empty after this dequeue
-            front = -1;
-            rear = -1;
+int getfront()
+{
+	int x=queue[front];
+	return x;
+}
+
+int getrear()
+{
+	int y=queue[rear];
+	return y;
+}
+
+void viewqueue()
+{
+
+ if (rear >= front) {
+ 	
+ 	printf("\n Queue Elements are :");
+            for (int i = front; i <= rear; i++) {
+                printf("%d ", queue[i]);
+            }
         } else {
-            front = (front + 1) % size_max;  // Circular increment
+            
+            for (int i = front; i < size_max; i++) {
+                printf("%d ", queue[i]);
+            }
+            
+            for (int i = 0; i <= rear; i++) {
+                printf("%d ", queue[i]);
+            }
         }
+        printf("\n");
     }
-}
-
-void viewqueue() {
-    if (isempty()) {
-        printf("\nQueue is empty.");
-        return;
-    }
-
-    printf("\nQueue elements are: ");
-    int i = front;
-    while (i != rear) {
-        printf("%d ", queue[i]);
-        i = (i + 1) % size_max;  // Circular traversal
-    }
-    printf("%d\n", queue[rear]);  // Print the last element
-}
-
-bool isfull() {
-    return ((rear + 1) % size_max == front);  // Queue is full if rear is just before front
-}
-
-bool isempty() {
-    return (front == -1);  // Queue is empty if front is -1
-}
